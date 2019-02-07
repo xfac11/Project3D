2e86247477c4a88aca124f22698d8b3321c4e454
kivoSSBM <37490094+kivoSSBM@users.noreply.github.com>
2019-02-07 17:44:53 +0100

    Add files via upload

---
+++ Testing D3D11/TestingDX11/Camera.cpp
@@ -0,0 +1,63 @@
+#include "Camera.h"
+
+Camera::Camera()
+{
+	this->position = DirectX::XMFLOAT3(0.f, 0.f, 0.f);
+	this->rotation = DirectX::XMFLOAT3(0.f, 0.f, 0.f);
+}
+
+Camera::~Camera()
+{
+}
+
+void Camera::SetPosition(float x, float y, float z)
+{
+	this->position = DirectX::XMFLOAT3(x, y, z);
+}
+
+void Camera::SetPosition(DirectX::XMFLOAT3 position)
+{
+	this->position = position;
+}
+
+void Camera::SetRotation(float x, float y, float z)
+{
+	this->rotation = DirectX::XMFLOAT3(x, y, z);
+}
+
+void Camera::SetRotation(DirectX::XMFLOAT3 rotation)
+{
+	this->rotation = rotation;
+}
+
+DirectX::XMFLOAT3 Camera::GetPosition()
+{
+	return this->position;
+}
+
+DirectX::XMFLOAT3 Camera::GetRotation()
+{
+	return this->rotation;
+}
+
+void Camera::Render()
+{
+	float radians = 0.0174532925f;
+
+
+	DirectX::XMVECTOR Up = DirectX::XMVectorSet(0, 1.f, 0, 0.0);
+	DirectX::XMVECTOR CamPos = DirectX::XMVectorSet(this->position.x, this->position.y, this->position.z, 0.0);
+	DirectX::XMVECTOR LookAt= DirectX::XMVectorSet(0.0, 0.0, 0.0, 0.0);
+	DirectX::XMFLOAT3 YawPitchRoll = DirectX::XMFLOAT3(this->rotation.x*radians, this->rotation.y*radians, this->rotation.z*radians);
+	DirectX::XMMATRIX rotationMatrix= DirectX::XMMatrixRotationRollPitchYaw(YawPitchRoll.x,YawPitchRoll.y,YawPitchRoll.z);
+	LookAt = DirectX::XMVector3TransformCoord(LookAt, rotationMatrix);
+	Up = XMVector3TransformCoord(Up, rotationMatrix);
+	LookAt = DirectX::XMVectorAdd(CamPos, LookAt);
+	this->viewMatrix =  DirectX::XMMatrixLookAtLH(CamPos, LookAt, Up);
+	this->viewMatrix = DirectX::XMMatrixTranspose(View); //maybe
+}
+
+DirectX::XMMATRIX& Camera::GetViewMatrix()
+{
+	return this->viewMatrix;
+}
--- Testing D3D11/TestingDX11/Camera.h
+++ Testing D3D11/TestingDX11/Camera.h
@@ -1,1 +1,25 @@
-#pragma once
+#ifndef CAMERA_H
+#define CAMERA_H
+#include <directxmath.h>
+class Camera
+{
+private:
+	DirectX::XMFLOAT3 position;
+	DirectX::XMFLOAT3 rotation;
+	//DirectX::XMFLOAT3 lookAt; 
+	DirectX::XMMATRIX viewMatrix;
+public:
+	Camera();
+	~Camera();
+	void SetPosition(float x, float y , float z);
+	void SetPosition(DirectX::XMFLOAT3 position);
+	void SetRotation(float x, float y , float z);
+	void SetRotation(DirectX::XMFLOAT3 rotation);
+
+	DirectX::XMFLOAT3 GetPosition();
+	DirectX::XMFLOAT3 GetRotation();
+
+	void Render();
+	DirectX::XMMATRIX& GetViewMatrix();
+};
+#endif
