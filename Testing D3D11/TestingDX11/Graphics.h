dfab700d71b3f4ed1601b6684e59e750e9ce761f
kivoSSBM <37490094+kivoSSBM@users.noreply.github.com>
2019-02-07 16:09:45 +0100

    Add files via upload

--- Testing D3D11/TestingDX11/Graphics.h
+++ Testing D3D11/TestingDX11/Graphics.h
@@ -1,1 +1,22 @@
-#pragma once
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+#include "D3D.h"
+const bool FULL_SCREEN = false; 
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+class Graphics
+{
+private:
+	D3D* Direct3D;
+
+	bool render(); //float [4]color
+public:
+	Graphics();
+	~Graphics();
+
+	bool Initialize(int screenWidth, int screenHeight, HWND hwnd);
+	void Shutdown();
+	bool Frame();
+};
+#endif
