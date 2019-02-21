d8ccd3ba8ca83b54bf5a1ecca9ef3012334bb973
xfac11 <xfac115@gmail.com>
2019-02-21 15:29:06 +0100

    Update Texture.cpp

--- Testing D3D11/TestingDX11/Texture.cpp
+++ Testing D3D11/TestingDX11/Texture.cpp
@@ -2,9 +2,9 @@
 
 Texture::Texture()
 {
-	//this->texture = nullptr;
+	//this->targaData = nullptr;
 	this->textureView = nullptr;
-	//this->DATA = nullptr;
+	this->DATA = nullptr;
 }
 
 Texture::~Texture()
@@ -12,47 +12,50 @@ Texture::~Texture()
 
 }
 
-
-//void Texture::setTexture2(ID3D11Device*& gDevice, ID3D11DeviceContext  *&gDeviceContext)
-//{
-//	ID3D11Texture2D* tex = nullptr;
-//	D3D11_SUBRESOURCE_DATA texInitData = { 0 };
-//	ZeroMemory(&texInitData, sizeof(texInitData));
-//	texInitData.pSysMem = (const void*)BTH_IMAGE_DATA;
-//	texInitData.SysMemPitch = BTH_IMAGE_WIDTH * 4;
-//	texInitData.SysMemSlicePitch = BTH_IMAGE_HEIGHT * BTH_IMAGE_WIDTH * 4;
-//
-//	D3D11_TEXTURE2D_DESC texDesc;
-//	ZeroMemory(&texDesc, sizeof(texDesc));
-//	texDesc.ArraySize = 1;
-//	texDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
-//	texDesc.CPUAccessFlags = 0;
-//	texDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-//	texDesc.Height = BTH_IMAGE_HEIGHT;
-//	texDesc.Width = BTH_IMAGE_WIDTH;
-//	//texDesc.Height = size_H;
-//	//texDesc.Width = size_W;
-//	texDesc.MipLevels = 1;
-//	texDesc.MiscFlags = 0;
-//	texDesc.SampleDesc.Count = 1;
-//	texDesc.SampleDesc.Quality = 0;
-//	texDesc.Usage = D3D11_USAGE_DEFAULT;
-//
-//
-//	gDevice->CreateTexture2D(&texDesc, &texInitData, &tex);
-//	gDevice->CreateShaderResourceView(tex, NULL, &this->textureView);
-//	//gDeviceContext->PSSetShaderResources(0, 1, &this->textureView);
-//}
-void Texture::setTexture(ID3D11Device*& gDevice, ID3D11DeviceContext *& gDeviceContext, char* filename)
+void Texture::setTexture2(ID3D11Device*& gDevice, ID3D11DeviceContext  *&gDeviceContext)
 {
-	if (!testTexture.Initialize(gDevice, gDeviceContext, filename))
-	{
-		std::string out = "\n\nFailed load texture:  ";
-		out+=filename;
-		out += "\n\n";
-		OutputDebugStringA(out.c_str());
-	}
-	
+	//this->targaData->setDATA(obj->getDATA(),obj->getSize());
+	//this->targaData->setWIDTH(obj->getWidth());
+	//this->targaData->setHEIGHT(obj->getHeight());
+	ID3D11Texture2D* tex = nullptr;
+	D3D11_SUBRESOURCE_DATA texInitData = { 0 };
+	ZeroMemory(&texInitData, sizeof(texInitData));
+	texInitData.pSysMem = (const void*)BTH_IMAGE_DATA;
+	texInitData.SysMemPitch = BTH_IMAGE_WIDTH * 4;
+	texInitData.SysMemSlicePitch = BTH_IMAGE_HEIGHT * BTH_IMAGE_WIDTH * 4;
+
+
+	D3D11_TEXTURE2D_DESC texDesc;
+	ZeroMemory(&texDesc, sizeof(texDesc));
+	texDesc.ArraySize = 1;
+	texDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+	texDesc.CPUAccessFlags = 0;
+	texDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	texDesc.Height = BTH_IMAGE_HEIGHT;
+	texDesc.Width = BTH_IMAGE_WIDTH;
+	texDesc.MipLevels = 1;
+	texDesc.MiscFlags = 0;
+	texDesc.SampleDesc.Count = 1;
+	texDesc.SampleDesc.Quality = 0;
+	texDesc.Usage = D3D11_USAGE_DEFAULT;
+
+	gDevice->CreateTexture2D(&texDesc, &texInitData, &tex);
+	gDevice->CreateShaderResourceView(tex, NULL, &this->textureView);
+}
+
+void Texture::setTexture(ID3D11Device*& gDevice, ID3D11DeviceContext *& gDeviceContext,std::string fileName)
+{
+	const char *file= fileName.c_str();
+	testTexture.Initialize(gDevice, gDeviceContext, file);
+	/*this->textureView = testTexture.GetTexture();
+
+	ID3D11Texture2D* tex = nullptr;
+	D3D11_SUBRESOURCE_DATA texInitData = { 0 };
+	ZeroMemory(&texInitData, sizeof(texInitData));
+	texInitData.pSysMem = (const void*)testTexture.getTextureCharArray();
+	texInitData.SysMemPitch = testTexture.getWidth() * 4;
+	texInitData.SysMemSlicePitch = testTexture.getHeight() * testTexture.getWidth() * 4;*/
+
 	ID3D11Texture2D* tex = nullptr;
 	D3D11_SUBRESOURCE_DATA texInitData = { 0 };
 	ZeroMemory(&texInitData, sizeof(texInitData));
@@ -77,6 +80,7 @@ void Texture::setTexture(ID3D11Device*& gDevice, ID3D11DeviceContext *& gDeviceC
 	gDevice->CreateTexture2D(&texDesc, &texInitData, &tex);
 	gDevice->CreateShaderResourceView(tex, NULL, &this->textureView);
 }
+
 ID3D11ShaderResourceView *& Texture::getTexture()
 {
 	return this->textureView;
@@ -84,8 +88,8 @@ ID3D11ShaderResourceView *& Texture::getTexture()
 
 void Texture::cleanUp()
 {
-	if (this->textureView)
+	if (this->textureView != nullptr)
+	{
 		this->textureView->Release();
-	//this->texture->Release();
-	this->testTexture.Shutdown();
+	}
 }
