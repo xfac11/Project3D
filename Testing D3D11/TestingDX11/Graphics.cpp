3c0dc792306dbb86a51ce95aeeaa420502e60d7f
xfac11 <xfac115@gmail.com>
2019-02-21 15:46:42 +0100

    Update Graphics.cpp

--- Testing D3D11/TestingDX11/Graphics.cpp
+++ Testing D3D11/TestingDX11/Graphics.cpp
@@ -1,6 +1,6 @@
 #include "Graphics.h"
 
-void Graphics::initImgui(HWND & hWnd)
+void Graphics::initImgui(HWND hWnd)
 {
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
@@ -9,13 +9,12 @@ void Graphics::initImgui(HWND & hWnd)
 	ImGui_ImplDX11_Init(this->Direct3D->GetDevice(), this->Direct3D->GetDeviceContext());
 	ImGui::StyleColorsDark();
 }
-
 void Graphics::move(Direction forward, Direction left_right, Direction up_down, bool flyMode, int mouseX, int mouseY)
 {
 	float ground_level = 0;
 	float gravity = -9.82f;
 	this->height = this->camPos.y - ground_level;
-	
+
 	float timePassed = sqrt(2 * height / abs(gravity));
 	float velocity = gravity * timePassed;
 
@@ -23,9 +22,9 @@ void Graphics::move(Direction forward, Direction left_right, Direction up_down, 
 	int sensitivity = 30;
 	this->camRot.x += mouseY * sensitivity* ImGui::GetIO().DeltaTime;
 	this->camRot.y += mouseX * sensitivity* ImGui::GetIO().DeltaTime;
-	
-	
-	if (this->camPos.y <= ground_level && up_down==Positive) //change this to collision checks with platforms
+
+
+	if (this->camPos.y <= ground_level && up_down == Positive) //change this to collision checks with platforms
 	{
 		this->isJumping = true;
 	}
@@ -34,13 +33,13 @@ void Graphics::move(Direction forward, Direction left_right, Direction up_down, 
 	if (flyMode == true)
 	{
 		this->camPos.y += (-this->camRot.x) * speed*forward; //up
-		this->camPos.y += speed * up_down*50;
+		this->camPos.y += speed * up_down * 50;
 	}
 	else //no flying
 	{
 		if (this->camPos.y > ground_level)
 		{
-			this->camPos.y += velocity*ImGui::GetIO().DeltaTime; //fall
+			this->camPos.y += velocity * ImGui::GetIO().DeltaTime; //fall
 		}
 
 		if (this->isJumping == true)
@@ -57,7 +56,7 @@ void Graphics::move(Direction forward, Direction left_right, Direction up_down, 
 			}
 		}
 	}
-	
+
 
 	//standard movement
 	//forward backward
@@ -71,7 +70,7 @@ void Graphics::move(Direction forward, Direction left_right, Direction up_down, 
 		this->camPos.z += (abs(this->camRot.y) - 90) * speed*-forward; //forward
 		this->camPos.x += (180 - abs(this->camRot.y))*(abs(this->camRot.y) / this->camRot.y) * speed*forward;  //leftright
 	}
-	
+
 	//left_right
 	if (abs(this->camRot.y) <= 90)
 	{
@@ -85,9 +84,8 @@ void Graphics::move(Direction forward, Direction left_right, Direction up_down, 
 		this->camPos.x += (abs(this->camRot.y) - 90) * speed*left_right*-1; //leftright
 
 	}
-	
-}
 
+}
 void Graphics::renderImgui()
 {
 	ImGui_ImplDX11_NewFrame();
@@ -107,7 +105,7 @@ void Graphics::renderImgui()
 
 	//textUse = "Fly - Mode[true / false]: [" + std::to_string();
 	//ImGui::Text(textUse);
-	textUse = "Height from 'Ground': " + std::to_string(this->height)+ "m";
+	textUse = "Height from 'Ground': " + std::to_string(this->height) + "m";
 	ImGui::Text(textUse.c_str());
 	ImGui::CaptureKeyboardFromApp(true);
 	//ImGui::ColorEdit4("Triangle data", (float*)gConstantBufferData);
@@ -115,22 +113,23 @@ void Graphics::renderImgui()
 	ImGui::End();
 }
 
-
-
 bool Graphics::render()
 {
 	this->renderImgui();
-	this->Direct3D->BeginScene(this->color);
+	Direct3D->BeginScene(this->color);
 	//Direct3D->initialize();
 	//campos mappedmemory
-	
-	//this->theModel->setVertexBuffer(this->Direct3D->GetDeviceContext());
+
+	//this->theCamera->SetPosition(xPos, yPos, dist);
+	//this->theCamera->SetRotation(xRot, yRot, 0.0f);
 	this->theCamera->SetPosition(this->camPos);
 	this->theCamera->SetRotation(this->camRot);
-	this->Direct3D->setIncrement(this->gIncrement);
+	//this->Direct3D->setIncrement(this->gIncrement);
 	this->theCamera->Render();
-	this->theModel[0].setWorld(DirectX::XMMatrixIdentity());
-	this->theModel[1].setWorld(DirectX::XMMatrixIdentity());
+
+	//Reset cube1World
+	this->theModel[0]->setWorld(DirectX::XMMatrixIdentity());
+	this->theModel[1]->setWorld(DirectX::XMMatrixIdentity());
 	//this->theModel[2].setWorld(DirectX::XMMatrixIdentity());
 
 	//DirectX::XMMATRIX Rotation;
@@ -142,34 +141,30 @@ bool Graphics::render()
 	////DirectX::XMMatrixRotationY
 	//Translation = DirectX::XMMatrixTranslation(0.0f, 0.0f, this->theModel[2].moveM);
 	//Set cube1's world space using the transformations
-	this->theModel[2].rotate(rotaxis, this->theModel[2].rot);
-	this->theModel[2].rot += 0.01f;
+	this->theModel[2]->rotate(rotaxis, this->theModel[2]->rot);
+	this->theModel[2]->rot += 0.01f;
 	//this->theModel[2]->move(1.0f, 0.0f, 0.0f);
-	if (this->theModel[2].rot > 3.141590f*2)
+	if (this->theModel[2]->rot > 3.141590f*2)
 	{
-		this->theModel[2].rot = 0;
+		this->theModel[2]->rot = 0;
 	}
-	this->theModel[2].setWorld();
-	this->theModel[1].setWorld();
-	this->theModel[0].setWorld();
+	this->theModel[2]->setWorld();
+	this->theModel[1]->setWorld();
+	this->theModel[0]->setWorld();
 
 	//this->Direct3D->setWorld(this->theModel[2].getId());
 	//this->theModel->setVertexBandTexture(this->Direct3D->GetDeviceContext());
 	//this->theModel[0].draw(*this->theColorShader, this->Direct3D->GetDeviceContext());
 	//this->theModel[1].draw(*this->theColorShader, this->Direct3D->GetDeviceContext());
-	this->theColorShader->SetShaderParameters(this->Direct3D->GetDeviceContext(), DirectX::XMLoadFloat4x4(&this->theModel[0].getId()), this->theCamera->GetViewMatrix(), this->Direct3D->GetProjectionMatrix());
-	this->theModel[0].draw(*this->theColorShader, this->Direct3D->GetDeviceContext());
-	this->theColorShader->SetShaderParameters(this->Direct3D->GetDeviceContext(), DirectX::XMLoadFloat4x4(&this->theModel[1].getId()), this->theCamera->GetViewMatrix(), this->Direct3D->GetProjectionMatrix());
-	this->theModel[1].draw(*this->theColorShader, this->Direct3D->GetDeviceContext());
-	this->theColorShader->SetShaderParameters(this->Direct3D->GetDeviceContext(), DirectX::XMLoadFloat4x4(&this->theModel[2].getId()), this->theCamera->GetViewMatrix(), this->Direct3D->GetProjectionMatrix());
-	this->theModel[2].draw(*this->theColorShader, this->Direct3D->GetDeviceContext());
-	//this->theColorShader->Render(this->Direct3D->GetDeviceContext(), this->theModel->getVertexCount(), this->Direct3D->GetWorldMatrix(), this->theCamera->GetViewMatrix(), this->Direct3D->GetProjectionMatrix());
-	
-	
+	this->theColorShader->SetShaderParameters(this->Direct3D->GetDeviceContext(), DirectX::XMLoadFloat4x4(&this->theModel[0]->getId()), this->theCamera->GetViewMatrix(), this->Direct3D->GetProjectionMatrix());
+	this->theModel[0]->draw(*this->theColorShader, this->Direct3D->GetDeviceContext());
+	this->theColorShader->SetShaderParameters(this->Direct3D->GetDeviceContext(), DirectX::XMLoadFloat4x4(&this->theModel[1]->getId()), this->theCamera->GetViewMatrix(), this->Direct3D->GetProjectionMatrix());
+	this->theModel[1]->draw(*this->theColorShader, this->Direct3D->GetDeviceContext());
+	this->theColorShader->SetShaderParameters(this->Direct3D->GetDeviceContext(), DirectX::XMLoadFloat4x4(&this->theModel[2]->getId()), this->theCamera->GetViewMatrix(), this->Direct3D->GetProjectionMatrix());
+	this->theModel[2]->draw(*this->theColorShader, this->Direct3D->GetDeviceContext());
 	ImGui::Render();
 	ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
 	Direct3D->EndScene();
-
 
 	return true;
 }
@@ -178,61 +173,68 @@ Graphics::Graphics()
 {
 	this->Direct3D = nullptr;
 	this->theCamera = nullptr;
-	this->theModel = new Model*[cap];
-	for (int i = 0; i < cap; i++)
-	{
-		this->theModel[i] = nullptr;
-	}
+	this->theModel = nullptr;
 	this->theColorShader = nullptr;
 	this->color[0] = 0.1f;
 	this->color[1] = 0.9f;
 	this->color[2] = 0.5f;
 	this->color[3] = 0.5f;
-	this->camPos = DirectX::XMFLOAT3(0.f, 0.f, -1.f);
-	this->camRot = DirectX::XMFLOAT3(0.f, 0.f, 0.f);
-	this->gIncrement = 0;
-	
+	this->cap = 4;
+	this->theModel = new Model*[cap];
+	for (int i = 0; i < cap; i++)
+	{
+		this->theModel[i] = nullptr;
+	}
+	//this->dist = -1.0f;
+	//this->xPos = 0.0f;
+	//this->xRot = 0.0f;
+	//this->yRot = 0.0f;
+	//this->yPos = 0.0f;
 	this->jumpTimer = 0.f;
 	this->isJumping = false;
 	this->height = 0.f;
+	this->camPos = DirectX::XMFLOAT3(0.f, 0.f, -1.f);
+	this->camRot = DirectX::XMFLOAT3(0.f, 0.f, 0.f);
+	this->gIncrement = 0;
 	this->rotaxis = DirectX::XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
 }
 
 Graphics::~Graphics()
 {
-	//this->Shutdown(); //????
+	this->Shutdown(); //????
 }
 
 bool Graphics::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 {
-	bool result;
+	bool result = false;
 	//if (this->Direct3D == nullptr)
 	//	throw std::bad_alloc();
 	this->Direct3D = new D3D;
-	if (Direct3D == nullptr)
+	if (this->Direct3D == nullptr)
 	{
 		MessageBox(hwnd, "Could not create Direct3D", "Error", MB_OK);
-		return false;
 	}
+	//this->Direct3D = (D3D*)::operator new (sizeof(D3D));
+	//this->Direct3D = (D3D*)_aligned_malloc(sizeof(D3D), 16);
 	result = Direct3D->initialize(screenWidth, screenHeight, VSYNC_ENABLED, hwnd, FULL_SCREEN, SCREEN_DEPTH, SCREEN_NEAR);
-	if (result==false)
+	if (!result)
 	{
 		MessageBox(hwnd, "Could not initialize Direct3D", "Error", MB_OK); //L"", L"", ;
 		result = false;
 	}
 	theCamera = new Camera;
 	//theCamera = (Camera*)_aligned_malloc(sizeof(Camera), 16);
-	if (theCamera==nullptr)
+	if (!theCamera)
 	{
 		MessageBox(hwnd, "My B, Could not initialize Camera", "Error", MB_OK);
 		result = false;
 	}
-	theCamera->SetPosition(0.0f, 0.0f, -1.0f);
+	theCamera->SetPosition(3.0f, 3.0f, 0.0f);
 
 	for (int i = 0; i < cap; i++) {
 		theModel[i] = new Model;
 	}
-	if (theModel==nullptr)
+	if (!theModel)
 	{
 		MessageBox(hwnd, "My B, Could not initialize Model", "Error", MB_OK);
 		result = false;
@@ -245,203 +247,81 @@ bool Graphics::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 	//}
 
 	theColorShader = new ColorShader;
-	if (theColorShader==nullptr)
+	if (!theColorShader)
 	{
-		MessageBox(hwnd, "Could not create the color shader object.", "Error", MB_OK);
 		return false;
 	}
 
 	// Initialize the color shader object.
 	result = theColorShader->Initialize(Direct3D->GetDevice(), hwnd);
-	if (result==false)
+	if (!result)
 	{
 		MessageBox(hwnd, "Could not initialize the color shader object.", "Error", MB_OK);
 		return false;
 	}
+	
+	this->theModel[0]->loadOBJ("stall.obj",this->Direct3D->GetDevice(), this->Direct3D->GetDeviceContext());
+	this->theModel[0]->setTheTexture(this->Direct3D->GetDevice(), this->Direct3D->GetDeviceContext(), "stallTexture.tga");
+	this->theModel[0]->createTheVertexBuffer(this->Direct3D->GetDevice());
+	this->theModel[1]->loadOBJ("eb_house_plant_01.obj", this->Direct3D->GetDevice(), this->Direct3D->GetDeviceContext());
+	this->theModel[1]->createTheVertexBuffer(this->Direct3D->GetDevice());
+	this->theModel[2]->addCube(DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f), 2, 2, 2);
+	this->theModel[2]->insertCubesInVec();
+	this->theModel[2]->setTheTexture(this->Direct3D->GetDevice(), this->Direct3D->GetDeviceContext(), "rock_01_dif_32bit.tga");
+	this->theModel[2]->createTheVertexBuffer(this->Direct3D->GetDevice());
+	this->theModel[0]->setSampler(this->Direct3D->GetDevice());
+	this->theModel[1]->setSampler(this->Direct3D->GetDevice());
+	this->theModel[2]->setSampler(this->Direct3D->GetDevice());
+	this->theModel[2]->setPosition(3.0f, 3.0f, 0.0f);
+	this->theModel[1]->setPosition(-10.0f, 0.0f, 0.0f);
+	this->theModel[0]->setPosition(0.0f, 10.0f, 0.0f);
 
-	//Vertex3D TwoTris[] = {
-	//	0.f, 0.f, 0.f,		//pos
-	//	0.0f, 1.0f,			//tex u & v
-	//	1.f, 1.f, 1.f,		//normal
-	//	-0.5f, -0.5f, 0.f,  //point
+	//this->theModel->setTheTexture(this->Direct3D->GetDevice(), this->Direct3D->GetDeviceContext(), "rock_01_dif_32bit");
 
-
-	//	0.f, 1.08f, 0.f,
-	//	0.f, 0.f,
-	//	1.f, 1.f, 1.f,
-	//	-0.5f, -0.5f, 0.f,
-
-	//	0.2133f, 1.08f, 0.f,
-	//	0.111f, 0.f,
-	//	1.f, 1.f, 1.f,
-	//	-0.5f, -0.5f, 0.f,
-
-	//
-
-	//	0.f, 0.f,0.f,
-	//	0.f, 1.f,
-	//	1.f, 1.f, 1.f,
-	//	-0.5f, -0.5f, 0.f,
-
-	//	0.2133f, 1.08f, 0.f,
-	//	0.111f, 0.f,
-	//	1.f, 1.f, 1.f,
-	//	-0.5f, -0.5f, 0.f,
-
-	//	0.2133f, 0.f, 0.0f,
-	//	0.111f, 1.f,
-	//	1.f, 1.f, 1.f,
-	//	-0.5f, -0.5f, 0.f,
-
-
-
-	//	0.2133f, 0.f, 0.f,
-	//	0.1111f, 1.f,
-	//	1.f, 1.f, 1.f,
-	//	-0.5f, -0.5f, 0.f,
-
-	//	0.2133f, 0.12f, 0.f,
-	//	0.111f, 0.8889f,
-	//	1.f, 1.f, 1.f,
-	//	-0.5f, -0.5f, 0.f,
-
-	//	1.7064f, 0.12f,0.f,
-	//	0.8889f, 0.8889f,
-	//	1.f, 1.f, 1.f,
-	//	-0.5f, -0.5f, 0.f,
-
-
-	//	0.2133f, 0.f, 0.f,
-	//	0.111f, 1.f,
-	//	1.f, 1.f, 1.f,
-	//	-0.5f, -0.5f, 0.f,
-
-	//	1.7067f, 0.12f, 0.f,
-	//	0.8889f, 0.8889f,
-	//	1.f, 1.f, 1.f,
-	//	-0.5f, -0.5f, 0.f,
-
-	//	1.7067f, 0.f, 0.f,
-	//	0.8889f, 1.f,
-	//	1.f, 1.f, 1.f,
-	//	-0.5f, -0.5f, 0.f,
-
-
-
-
-	//					//-0.5f, -0.5f, 0.f,	//pos
-	//					////0.f, 0.f, 0.f,	//color
-	//					//0.0f, 1.0f,			//tex u & v
-	//					//1.f, 1.f, 1.f,		//normal
-	//					//-0.5f, -0.5f, 0.f,  //point
-
-	//					//-0.5f, 0.5f, 0.f,
-	//					//0.f, 0.f,
-	//					//1.f, 1.f, 1.f,
-	//					//-0.5f, 0.5f, 0.f,
-
-	//					//0.5, 0.5, 0.0,
-	//					//1.0, 0.0,
-	//					//1.f, 1.f, 1.f,
-	//					//0.5, 0.5, 0.0,
-
-	//					//-0.5, -0.5, 0.0,
-	//					//0.0, 1.0,
-	//					//1.f, 1.f, 1.f,
-	//					//-0.5, -0.5, 0.0,
-
-	//					//0.5f, 0.5f, 0.f,
-	//					//1.0, 0.0,
-	//					//1.f, 1.f, 1.f,
-	//					//-0.5f, -0.5f, 0.0,
-
-	//					//0.5f, -0.5f, 0.f,
-	//					//1.f, 1.f,
-	//					//1.f, 1.f, 1.f,
-	//					//0.5f, -0.5f, 0.f,
-	//};
-	//Vertex3D triangleVertices[6] =
-	//{
-	//	1.92f, 0.f, 3.0f,	//v0 pos
-	//	//1.0f, 0.0f, 0.0f,   //v0 color
-	//	1.0f, 1.0f,//v0 textureCoord
-	//	1.0f,1.0f,1.0f,
-	//	0.5f, -0.5f, 0.0f,
-
-	//	0.f, 1.08f, 3.0f,	//v1
-	//	//1.0f 0.0f, 0.0f,	//v1 color
-	//	0.0f, 0.0f,         //v0 textureCoord 
-	//	1.0f,1.0f,1.0f,
-	//	-0.5f, 0.5f, 0.0f,
-
-	//	1.92f, 1.08f, 3.0f, //v2
-	//	//1.0f, 0.0f, 0.0f,	//v2 color
-	//	1.0f, 0.0f,
-	//	1.0f,1.0f,1.0f,
-	//	0.5f, 0.5f, 0.0f,
-
-	//	0.f, 1.08f, 3.0f,	//v3 pos
-	//	//0.0f, 0.0f, 1.0f,	//v0 color
-	//	0.0f, 0.0f,
-	//	1.0f,1.0f,1.0f,
-	//	-0.5f, +0.5f, 0.0f,
-
-	//	1.92f, 0.f, 3.0f,	//v4
-	//	//0.0f, 0.0f, 1.0f,	//v1 color
-	//	1.0f, 1.0f,
-	//	1.0f,1.0f,1.0f,
-	//	0.5f, -0.5f, 0.0f,
-
-	//	0.f, 0.f, 3.0f, //v5
-	//	//0.0f, 0.0f, 1.0f,	//v2 color
-	//	0.0f, 1.0f,
-	//	1.0f,1.0f,1.0f,
-	//	-0.5f, -0.5f, 0.0f,
-	//};
-	//size_t nrOfTwoTris = sizeof(TwoTris) / sizeof(TwoTris[0]);
-	//this->theModel->addQuads(TwoTris, nrOfTwoTris, this->Direct3D->GetDevice());
-	//size_t nrTriVer = sizeof(triangleVertices) / sizeof(triangleVertices[0]);
-	//this->theModel->addQuads(triangleVertices, nrTriVer, this->Direct3D->GetDevice());
-	//this->theModel->createTheVertexBuffer(this->Direct3D->GetDevice());
-
-	this->theModel[0].loadOBJ("stall.obj",this->Direct3D->GetDevice(), this->Direct3D->GetDeviceContext());
-	this->theModel[0].setTheTexture(this->Direct3D->GetDevice(), this->Direct3D->GetDeviceContext(), "stallTexture.tga");
-	this->theModel[0].createTheVertexBuffer(this->Direct3D->GetDevice());
-	this->theModel[1].loadOBJ("eb_house_plant_01.obj", this->Direct3D->GetDevice(), this->Direct3D->GetDeviceContext());
-	this->theModel[1].createTheVertexBuffer(this->Direct3D->GetDevice());
-	this->theModel[2].addCube(DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f), 2, 2, 2);
-	this->theModel[2].insertCubesInVec();
-	this->theModel[2].setTheTexture(this->Direct3D->GetDevice(), this->Direct3D->GetDeviceContext(), "rock_01_dif_32bit.tga");
-	this->theModel[2].createTheVertexBuffer(this->Direct3D->GetDevice());
-	this->theModel[0].setSampler(this->Direct3D->GetDevice());
-	this->theModel[1].setSampler(this->Direct3D->GetDevice());
-	this->theModel[2].setSampler(this->Direct3D->GetDevice());
-	this->theModel[2].setPosition(3.0f, 3.0f, 0.0f);
-	this->theModel[1].setPosition(-10.0f, 0.0f, 0.0f);
-	this->theModel[0].setPosition(0.0f, 10.0f, 0.0f);
 	return result;
 }
 
 void Graphics::Shutdown()
-{
+{/*
+	if (Direct3D)
+	{
+		Direct3D.Shutdown();
+		delete Direct3D;
+		Direct3D = NULL;
+	}*/
+	if (Direct3D)
+	{
+		_aligned_free(Direct3D);
+	}
+	for (int i = 0; i < cap; i++)
+	{
+		this->theModel[i]->shutdown();
+		delete this->theModel[i];
+	}
+	delete[] this->theModel;
+	delete this->theColorShader;
+	delete this->Direct3D;
+	delete this->theCamera;
+
+	Direct3D->Shutdown();
 	ImGui_ImplDX11_Shutdown();
 	ImGui_ImplWin32_Shutdown();
 	ImGui::DestroyContext();
-
-	this->Direct3D->Shutdown();
-	this->theModel[0]->shutdown();
-	this->theModel[1]->shutdown();
-	this->theColorShader->Shutdown();
-
-	delete Direct3D;
-	delete theCamera;
-	delete theModel[0];
-	delete theModel[1];
-	delete[] theModel;
-	delete theColorShader;
 }
 
-bool Graphics::Frame()
+bool Graphics::Frame(unsigned char key)
 {
+	if (key == 'K')
+	{
+		this->theModel[2]->move(-1.0f, 0.0f, 0.0f);
+	}
+	if (key == 'J')
+	{
+		//this->theModel[2].rotate(1.14);
+		this->theModel[2]->move(1.0f, 0.0f, 0.0f);
+		//this->theModel[2].createTheVertexBuffer(this->Direct3D->GetDevice());
+		//this->Direct3D->setIncrement(3.14);// = this->theModel[2].getId();
+	}
+	
 	return render();
 }
