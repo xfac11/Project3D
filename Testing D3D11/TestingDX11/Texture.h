082990f68620cb882ff8a9dce2d248d154b80a34
xfac11 <xfac115@gmail.com>
2019-02-21 15:29:47 +0100

    Update Texture.h

--- Testing D3D11/TestingDX11/Texture.h
+++ Testing D3D11/TestingDX11/Texture.h
@@ -1,8 +1,8 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
 #include <d3d11.h>
-//#pragma comment(lib, "d3d11.lib")
 #include <directxmath.h>
+//#include"Structs.h"
 #include "bth_image.h"
 #include "textureload.h"
 //#include "TextureData.h"
@@ -11,20 +11,17 @@ class Texture
 public:
 	Texture();
 	~Texture();
-	//void setTexture2(ID3D11Device*& gDevice, ID3D11DeviceContext *& gDeviceContext);
-	void setTexture(ID3D11Device*& gDevice, ID3D11DeviceContext *& gDeviceContext, char* filename);
+	void setTexture2(ID3D11Device*& gDevice, ID3D11DeviceContext *& gDeviceContext);
+	void setTexture(ID3D11Device*& gDevice, ID3D11DeviceContext *& gDeviceContext, std::string fileName);
 	//void changeTextureData(TextureData* obj);
 	ID3D11ShaderResourceView*& getTexture();
 	void cleanUp();
 private:
 	//ID3D11Texture2D* texture;
 	ID3D11ShaderResourceView* textureView;
-	//ID3D11Resource* texture;
-	//ID3D11CreateTextureFromFile* texureTest;
 	//TextureData* targaData;
-	//unsigned char* DATA;
+	unsigned char* DATA;
 	TextureLoad testTexture;
-
 
 };
 #endif // !TEXTURE_H
