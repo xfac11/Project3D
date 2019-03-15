84e56a86bdb0a59b7bba7c037cbac52adf1c05f5
xfac11 <xfac115@gmail.com>
2019-03-15 15:37:07 +0100

    Update Model.h

--- Testing D3D11/TestingDX11/Model.h
+++ Testing D3D11/TestingDX11/Model.h
@@ -7,6 +7,7 @@
 #include"QuadHandler.h"
 #include"Texture.h"
 #include "Loader.h"
+#include"DefferedShader.h"
 #include"ColorShader.h"
 class Model
 {
@@ -26,14 +27,17 @@ public:
 	void setTheTexture( ID3D11Device*& gDevice, ID3D11DeviceContext *&gDeviceContext, std::string fileName);
 	void setSampler(ID3D11Device*& gDevice);
 	void loadOBJ(char* file, ID3D11Device* device, ID3D11DeviceContext* deviceContext);
+	void draw(DefferedShader &shader, ID3D11DeviceContext * deviceContext);
 	void draw(ColorShader &shader, ID3D11DeviceContext * deviceContext);
 	void rotate(float angle);
 	DirectX::XMFLOAT4X4 getId();
 	void setWorld(DirectX::XMMATRIX mtrx);
 	void setWorld();
 	void setPosition(float x, float y, float z);
+	DirectX::XMFLOAT3 getPosition();
 	void move(float x, float y, float z);
 	void rotate(DirectX::XMVECTOR axis, float angle);
+	void setScale(float x, float y, float z);
 	float rot;
 	float moveM;
 private:
@@ -47,11 +51,13 @@ private:
 	DirectX::XMFLOAT4X4 Scale;
 	DirectX::XMFLOAT4X4 Translation;
 	DirectX::XMFLOAT3 position;
+	DirectX::XMFLOAT3 scale;
 	Texture texture;
 	Loader load;
 	int vertexCount;
 	//int indexCount;
 	QuadHandler quads;
 	CubeHandler cubes;
+
 };
 #endif // !MODEL_H
